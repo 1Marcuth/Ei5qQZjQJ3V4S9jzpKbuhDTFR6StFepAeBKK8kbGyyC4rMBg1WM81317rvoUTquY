cb94e8c3186042449b5d59726ebb26e04b8ad16f
Marcuth <91915075+1Marcuth@users.noreply.github.com>
2024-05-15 00:24:30 -0300

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -4,13 +4,12 @@ const axios = require('axios');
 async function fetchData() {
   const userId = process.env.USER_ID;
   const authToken = process.env.AUTH_TOKEN;
+  const endpointUrl = process.env.ENDPOINT_URL;
 
   try {
-    const response = await axios.post(process.env.ENDPOINT_URL, {
-      params: {
-        userId: userId,
-        authToken: authToken
-      }
+    const response = await axios.post(endpointUrl, {
+      userId: userId,
+      authToken: authToken
     });
 
     fs.writeFileSync('data.json', JSON.stringify(response.data, null, 2));
